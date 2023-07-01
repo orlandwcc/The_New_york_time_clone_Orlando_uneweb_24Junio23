6fa5e4b8ef2e2a0e3ad00a0556c3bd0fb88b592b
orlandwcc <orlandwcc@gmail.com>
2023-07-01 16:02:18 -0400

    modific

--- Formulario/css/js.js
+++ Formulario/css/js.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function() {
     showPassword = document.querySelector('.show-password');
     showPassword.addEventListener('click', () => {
- 
+
       password = document.querySelector('.password');
- 
+
       if ( password.type === "text" ) {
         password.type = "password"
         showPassword.classList.remove('fa-eye-slash');
@@ -12,4 +12,14 @@ window.addEventListener("load", function() {
         showPassword.classList.toggle("fa-eye-slash");
       }
   })
+});
+
+var required = document.getElementById('required');
+required.addEventListener('required', function(evt) {
+  this.setCustomValidity('');
+});
+required.addEventListener('invalid', function(evt) {
+  if (this.validity.valueMissing) {
+    this.setCustomValidity('Por favor complete este campo');
+  }
 });
